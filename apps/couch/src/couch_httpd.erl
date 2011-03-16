b2bf5571b3eebb070726c3e6eb01f95c244a15b5
Adam Kocoloski <adam@cloudant.com>
2011-03-16 17:14:49 -0400

    Suppress duplicate access logs

--- apps/couch/src/couch_httpd.erl
+++ apps/couch/src/couch_httpd.erl
@@ -533,13 +533,8 @@ verify_is_server_admin(#user_ctx{roles=Roles}) ->
     false -> throw({unauthorized, <<"You are not a server admin.">>})
     end.
 
-log_request(#httpd{mochi_req=MochiReq,peer=Peer}, Code) ->
-    ?LOG_INFO("~s - - ~p ~s ~B", [
-        Peer,
-        couch_util:to_existing_atom(MochiReq:get(method)),
-        MochiReq:get(raw_path),
-        couch_util:to_integer(Code)
-    ]).
+log_request(_, _) ->
+    noop. % we have twig
 
 
 start_response_length(#httpd{mochi_req=MochiReq}=Req, Code, Headers, Length) ->
