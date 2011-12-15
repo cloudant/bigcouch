017ba1c626e565d3b70c90ca92496d2602e4a909
Adam Kocoloski <adam@cloudant.com>
2011-12-15 17:03:47 -0500

    Mark proc as unused in ret_proc
    Otherwise we'll never reuse it

--- apps/couch/src/couch_proc_manager.erl
+++ apps/couch/src/couch_proc_manager.erl
@@ -68,7 +68,7 @@ handle_call({ret_proc, #proc{client=Ref, pid=Pid} = Proc}, _From, State) ->
     % table before the insert.  Don't know which approach is cheaper.
     case is_process_alive(Pid) of true ->
         gen_server:cast(Pid, garbage_collect),
-        ets:insert(State#state.tab, Proc);
+        ets:insert(State#state.tab, Proc#proc{client=nil});
     false -> ok end,
     {reply, true, State};
 
